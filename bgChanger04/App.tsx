d387bdd07bd74c21632b18b019c8a13b5527027b
Shantanu-0101 <spanchal112000@gmail.com>
2026-06-11 11:29:11 +0530

    Currency Converter Project

--- bgChanger04/App.tsx
+++ bgChanger04/App.tsx
@@ -1,45 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, TouchableOpacity, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { useState } from 'react';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [randomBackground, setRandomBackground] = useState("#ffffff")
+
+  const generateColor = () => {
+    const hexRange ="0123456789ABCDEF"
+    let color = '#'
+
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)]
+      
+    }
+
+    setRandomBackground(color)
+  }
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <>
+    <StatusBar backgroundColor={randomBackground}/>
+    <View style={[styles.container, {backgroundColor: randomBackground}]}>
+      <TouchableOpacity onPress={generateColor}>
+        <View style={styles.actionButton}>
+          <Text style={styles.actionButtonTxt}>Press Me</Text>
+        </View>
+      </TouchableOpacity>
     </View>
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center'
   },
+
+  actionButton: {
+    borderRadius: 12 ,
+    backgroundColor: "#6A1B4D",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    margin: 80
+
+   },
+
+   actionButtonTxt: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
+    alignItems:'center',
+    justifyContent: 'center',
+    paddingLeft: 30
+   }
+
+
 });
 
 export default App;
